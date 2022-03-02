ca9097164582d15c2f3bcb937881db167fd80a3d
Jojo Bautista <gjojob@yahoo.com>
2022-03-02 17:43:15 -0600

    added queries for thought, friend and reactions

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,5 @@
 import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
 export const QUERY_THOUGHTS = gql`
   query thoughts($username: String) {
@@ -13,6 +14,45 @@ export const QUERY_THOUGHTS = gql`
         createdAt
         username
         reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_THOUGHT = gql`
+  query thought($id: ID!) {
+    thought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
       }
     }
   }
