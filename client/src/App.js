62de6ad2d9cd8291953baa8f5eae30ae19f4cd37
Jojo Bautista <gjojob@yahoo.com>
2022-03-02 16:04:47 -0600

    Added concurrent functionality

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,12 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// const httpLink = createHttpLink({
+//   uri: 'http://localhost:3001/graphql',
+// });
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
